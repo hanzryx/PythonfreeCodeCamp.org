f14e8ba1d11e139ce84cf5b151806ea9f990510d
Hanz Zaidan <hikmalarkannurzaidan@gmail.com>
2021-10-31 14:07:17 +0700

    Add files via upload

---
+++ Variabel&TipeData.py
@@ -0,0 +1,11 @@
+character_name = "Udin"
+character_age = "25"
+is_male = False
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+character_name = "Beni"
+character_age = "18.69"
+
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ". ")
